6e894cdbdebcef36eaf57972f69f0b0a8bf6ec97
Átila de Freitas <atila.defreitas@icloud.com>
2024-02-11 21:25:49 -0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import '@vercel/analytics';
+
 
 import { ThemeProvider } from '@material-tailwind/react';
 
